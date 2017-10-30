6ded59211b5d5377ac534eec8e50f1192b9b0309
Liam Killion <liam.killion@gmail.com>
2017-10-29 20:27:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Liam"
+  end
+
+  get '/hometown' do
+    "My hometown is Branford, CT"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Purple Rain by Prince"
+  end
 end
